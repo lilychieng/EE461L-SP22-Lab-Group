688532f258aa41442233cbf06020ad731da36da6
mvu <54677355+mhvu2023@users.noreply.github.com>
2022-03-30 14:56:52 -0500

    Implemented Flask route functionality for user signup and login

--- Backend/base.py
+++ Backend/base.py
@@ -1,13 +1,13 @@
-from flask import Flask, redirect, url_for, request
+from flask import Flask, redirect, url_for, request, jsonify
 from pymongo import MongoClient
 from cryptography.fernet import Fernet
 from configparser import ConfigParser
 from hashlib import sha256
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 from Users import Users
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, supports_credentials=True)
 config = ConfigParser()
 c = ""
 
@@ -17,48 +17,63 @@ def index():
    return 'Hello world'
 
 '''
+Route: signup
 
+Creates new user and registers them within the `Users` database
+
+Returns:
+400 - Server received malformed username or password formatting
+500 - Internal server failed to register within the `Users` database
+200 - Succesful user registration in the database
 '''
-@app.route('/user/signup/', methods=["GET"])
+@app.route('/user/signup/', methods=["POST"])
+@cross_origin(supports_credentials=True)
 def signup():
    print(f"Routing works", flush=True)
    payload = request.args.to_dict()
-
+   
    username = payload['username']                                       #Plaintext username
    password = sha256(payload['password'].encode('UTF-8')).hexdigest()   #Hashed password
-
+   
    user = Users(username, password)                                     #User object
-
+   
    #TODO: 300 code for existing user, do user validation later
-
+   
    try:
       t_id = c.Checkout.Users.insert_one(user.to_database()).inserted_id
    except:
       return "failed to register user", 500
    else:
-      print(f"User registered, transaction id is {t_id}", flush=True)
       return "successfully registered", 200
-
-
-'''
+   
 
 '''
-@app.route('/user/login/', methods=["GET"])
+Route: login
+
+Searches for first match of provided email (unique identifier) in the database, then
+verifies password hashes match with database entry.
+
+Returns:
+404 - User email in credentials do not match any existing entry
+401 - User password in credentials does not match an existing entry
+200 - Successful user login
+'''
+@app.route('/user/login', methods=["GET"])
 def login():
    payload = request.args.to_dict()
 
    username = payload['username']                                       #Plaintext username
    password = sha256(payload['password'].encode('UTF-8')).hexdigest()   #Hashed password
-   
-   print(f"Entered user credentials: {username}, {password}", flush=True)
 
    collection = c.Checkout.Users
    matched = collection.find_one({'user': username})
    
-   if (matched['password'] == password):
+   if matched == None:
+      return 'user not found', 404
+   elif (matched['password'] == password):
       return 'success', 200
    else:
-      return 'user not found', 404
+      return 'user credentials do not match', 401
 
 
 if __name__ == '__main__':
