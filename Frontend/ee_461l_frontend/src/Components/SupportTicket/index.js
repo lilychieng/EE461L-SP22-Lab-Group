f77fe57ce9f5fa189c52dca72641bdd7c72125bb
Lily Chieng <lilychieng556@gmail.com>
2022-03-30 15:06:51 -0500

    fixed support form

--- Frontend/ee_461l_frontend/src/Components/SupportTicket/index.js
+++ Frontend/ee_461l_frontend/src/Components/SupportTicket/index.js
@@ -10,9 +10,10 @@ import { styled } from "@mui/material/styles";
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
   ...theme.typography.body2,
-  padding: theme.spacing(1),
+  padding: theme.spacing(0.25),
   textAlign: "center",
   color: theme.palette.text.secondary,
+  boxShadow: "none",
 }));
 
 const issues = [
